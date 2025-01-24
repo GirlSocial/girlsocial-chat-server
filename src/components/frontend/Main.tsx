3e38321a65a2f53f49daa690b69667a1f6d35907
Emily <mldkyt@proton.me>
2025-01-24 17:24:38 +0100

    chore: Inter font

--- src/components/frontend/Main.tsx
+++ src/components/frontend/Main.tsx
@@ -1,10 +1,13 @@
 import {ReactNode} from "react";
 import {CssBaseline, CssVarsProvider} from "@mui/joy";
+import {Inter} from "next/font/google";
+
+const inter = Inter({subsets: ['latin']})
 
 export default function (props: {children: ReactNode}) {
 
     return <CssVarsProvider defaultColorScheme={'dark'}>
-        <main className={'w-2/3 m-auto'}>
+        <main className={`w-2/3 m-auto ${inter.className}`}>
             <CssBaseline />
             {props.children}
         </main>
